c7aba6bf316592ce8016d8cbba431a3a032c0547
Sanam Maharjan <maharjansanam027@gmail.com>
2025-02-05 16:20:53 +0545

    generic template v.0

--- Day2/classes.js
+++ Day2/classes.js
@@ -7,9 +7,18 @@ class Person {
         console.log(`hi ${this.name}`);
     }
 }
+// public private protected
+class Customer extends Person {
+    constructor(salary, name, age) {
+        super(name, age);
+        this.salary1 = salary;
+    }
+}
 const p1 = new Person("sanam", 25);
 const p2 = new Person("rohit", 20);
+const p3 = new Customer(25000, "negi", 23);
 console.log(p1);
 console.log(p2);
 p1.greet2();
+console.log(p3);
 // console.log(p1.greet1())
--- Day2/classes.ts
+++ Day2/classes.ts
@@ -13,12 +13,41 @@ class Person{
 
 }
 
+
+// public private protected
+class Customer extends Person{
+    salary1:number;
+
+    constructor(salary:number,name:string,age:number){
+        super(name,age);
+        this.salary1 = salary;
+    }
+
+
+}
+
 const p1 = new Person("sanam",25);
 const p2 = new Person("rohit",20);
+const p3 = new Customer(25000,"negi",23);
 
 console.log(p1);
 console.log(p2);
 
 p1.greet2();
 
+console.log(p3);
+
 // console.log(p1.greet1())
+
+// Generic Template
+
+// function value(val:(number | string | number[])):(number | string | number[]){
+//     return val;
+// }
+
+function value<T>(a:T):T{
+    return a;
+}
+
+console.log(value("sanam"));
+console.log(value([2,6,5,8,4,2,6]));
